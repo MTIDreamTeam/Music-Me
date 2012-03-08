273a5529ebe2968fdf36a765e9bb7902338a7d9f
JC Lanoë <contact@lanoe.me>
2012-03-08 13:37:37 +0100

    Initiated a Symfony test Application.

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -3,11 +3,13 @@
 namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class WelcomeController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
+		// return new Response('<html><body><h1>Hello</h1></body></html>');
     }
 }
