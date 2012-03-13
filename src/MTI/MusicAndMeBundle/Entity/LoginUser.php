1234180142a0dafa569b993562e08166776f8f2c
JC Lanoë <contact@lanoe.me>
2012-03-13 19:26:39 +0100

    Added a model for login forms.

---
+++ src/MTI/MusicAndMeBundle/Entity/LoginUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MTI\MusicAndMeBundle\Entity;
+
+
+class LoginUser
+{
+    protected $email;
+    protected $password;
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
