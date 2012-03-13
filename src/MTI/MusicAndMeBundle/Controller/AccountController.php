4c676540a1b92821f8ed8f7bf12f572fb7b9ceb2
JC Lanoë <contact@lanoe.me>
2012-03-13 19:27:32 +0100

    Account creation/login/logout functional.

--- src/MTI/MusicAndMeBundle/Controller/AccountController.php
+++ src/MTI/MusicAndMeBundle/Controller/AccountController.php
@@ -5,28 +5,33 @@ namespace MTI\MusicAndMeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\SessionStorage\PdoSessionStorage;
+
 use MTI\MusicAndMeBundle\Entity\User;
-use Symfony\Component\Yaml\Yaml;
+use MTI\MusicAndMeBundle\Entity\LoginUser;
+use MTI\MusicAndMeBundle\Security\Authentication;
 
 
 class AccountController extends Controller
 {
 	public function indexAction(Request $request)
 	{
-		$user = new User();
-
-		$form = $this->createFormBuilder($user)->add('firstname', 'text')
-												->add('lastname', 'text')
-												->add('email', 'email')
-												->add('password', 'password')
-												->getForm();
-
-		return $this->render(
-			'MTIMusicAndMeBundle:Account:create.html.twig',
-			array(
-				'form' => $form->createView()
-			)
-		);
+		if (!Authentication::isAuthenticated($request))
+			return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_login'), 301);
+		
+		$session = $this->get('session');
+		// $session->set('user_id', '1234');
+		// 
+		// if ($session->get('user_id') == null)
+		// {
+		// 	return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_login'), 301);
+		// }
+		
+		$user = $this->getDoctrine()
+								->getRepository('MTIMusicAndMeBundle:User')
+								->find($session->get('user_id'));
+		
+		return new Response("Hello ".$user->getFirstname() . ' ' . $user->getLastname());
 	}
 	
 	public function createAction(Request $request)
@@ -37,24 +42,7 @@ class AccountController extends Controller
 												->add('email', 'email')
 												->add('password', 'password')
 												->getForm();
-
-		return $this->render(
-			'MTIMusicAndMeBundle:Account:create.html.twig',
-			array(
-				'form' => $form->createView()
-			)
-		);
-	}
-	
-	public function validateAction(Request $request)
-	{
-		$user = new User();
-		$form = $this->createFormBuilder($user)->add('firstname', 'text')
-												->add('lastname', 'text')
-												->add('email', 'email')
-												->add('password', 'password')
-												->getForm();
-
+		
 		if ($request->getMethod() == 'POST')
 		{
 			$form->bindRequest($request);
@@ -77,30 +65,104 @@ class AccountController extends Controller
 				$em->persist($user);
 				$em->flush();
 				
-				return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_create'), 302);
+				return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_account'), 302);
 			}
 		}
 		else
 		{
-			return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_create'), 302);
+			return $this->render(
+				'MTIMusicAndMeBundle:Account:create.html.twig',
+				array(
+					'form' => $form->createView()
+				)
+			);
 		}
 	}
 	
 	public function loginAction(Request $request)
 	{
-		$user = new User();
+		if (Authentication::isAuthenticated($request))
+			return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_account'), 301);
+		
+		$user = new LoginUser();
 
-		$form = $this->createFormBuilder($user)->add('firstname', 'text')
-												->add('lastname', 'text')
-												->add('email', 'email')
+		$form = $this->createFormBuilder($user)->add('email', 'email')
 												->add('password', 'password')
 												->getForm();
+		
+		if ($request->getMethod() == 'POST')
+		{
+			$form->bindRequest($request);
+			
+			$validator = $this->get('validator');
+			$errors = $validator->validate($user);
 
-		return $this->render(
-			'MTIMusicAndMeBundle:Account:create.html.twig',
-			array(
-				'form' => $form->createView()
-			)
-		);
+			// Found some errors in the login form
+			if (count($errors) > 0)
+			{
+				return $this->render(
+					'MTIMusicAndMeBundle:Account:login.html.twig',
+					array(
+						'form' => $form->createView()
+					)
+				);
+			}
+			// There is no semantical errors in the form
+			else
+			{
+				$registeredUser = $this->getDoctrine()
+										->getRepository('MTIMusicAndMeBundle:User')
+										->findBy(
+											array('email' => $form->getData()->getEmail())
+										);
+				// return new Response(count($registeredUser) == 1 ? 'yes' : 'no');
+				if (count($registeredUser) == 0 || $registeredUser[0]->getPassword() != $form->getData()->getPassword())
+				{
+					return $this->render(
+						'MTIMusicAndMeBundle:Account:login.html.twig',
+						array(
+							'form' => $form->createView(),
+							'login_error' => 'Email ou mot de passe incorrect'
+						)
+					);
+				}
+				
+				$session = $this->get('session');
+				$route = $session->get('nextRoute');
+				$session->set('nextRoute', 'MTIMusicAndMeBundle_account');
+				$session->set('user_id', $registeredUser[0]->getId());
+				// return new Response('route : ');
+				
+				return $this->redirect($this->generateUrl($route), 301);
+			}
+		}
+		else
+		{
+			return $this->render(
+				'MTIMusicAndMeBundle:Account:login.html.twig',
+				array(
+					'form' => $form->createView()
+				)
+			);
+		}
+	}
+	
+	public function logoutAction(Request $request)
+	{
+		// Log the user out
+		$session = $this->get('session');
+		$session->invalidate();
+		$this->get("security.context")->setToken(null);
+		
+		return new Response("session invalidated : ".$session->get('user_id'));
+		
+		return new Response($request->attributes->get('_route'));
+		
+		if ($session)
+		{
+			return $this->redirect($this->generateUrl('MTIMusicAndMeBundle_login'), 301);
+		}
+		
+		return new Response("Failed to logout");
 	}
 }
